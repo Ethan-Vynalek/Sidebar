67044aefc5d7c5bf830ef9ce84359064426b4650
pobb001 <pobb001@bucknell.edu>
2015-09-11 15:53:18 -0400

    done work for the day

--- sidebar.js
+++ sidebar.js
@@ -1,3 +1,11 @@
 console.log("Start of Extension");
 
+// Table Headers
+$("tbody th:last").css("padding-left", "20px");
+
+
+// Add to Calendar Button
 $("tr input.button").after('<input type="submit" value="Add to Calendar" class="button calendar"></input>');
+
+var $lastTD = $("tr").find("td:last")
+$lastTD[22].children[0].tagName
