e39556e9e4048b95b9b6d47913eea523c5afb22a
Patrick Bacon-Blaber <pablaber225@gmail.com>
2015-09-13 21:24:46 -0400

    added 'Add to Calendar' button

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,18 @@
-console.log("Start of Extension");
+console.log("Start of Extension"); // for debugging purposes
 
 // Table Headers
-$("tbody th:last").css("padding-left", "20px");
+// $("tbody th:last").css("padding-left", "20px");
 
+// Table header
+$("#coursetable tbody tr:first th:last").after("<th class='ddlabel' scope='row'>Add To<br>Calendar</th>");
 
 // Add to Calendar Button
-$("tr input.button").after('<input type="submit" value="Add to Calendar" class="button calendar"></input>');
-
-var $lastTD = $("tr").find("td:last")
-$lastTD[22].children[0].tagName
+var $lastTD = $("tr").find("td:last");
+$lastTD.each(function(index) {
+	if($(this).children(0).prop("tagName") === "FONT") {
+		$(this).attr("colspan", "14");
+	}
+	else if($(this).children(0).prop("tagName") === "FORM"){
+		$(this).after("<input type='submit' value='Add to Calendar' class='button'>");
+	}
+});
