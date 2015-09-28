74db012a784bae59dd1435fbe4e06988245c1134
pobb001 <pobb001@bucknell.edu>
2015-09-28 16:22:00 -0400

    add class object parameter formatting

--- sidebar.js
+++ sidebar.js
@@ -32,17 +32,17 @@ $lastTD.each(function(index) {
 $(".atcbutton").click(function(){
 	var tds = $(this).parent("tr").children();
 	var classObj = {};
-	classObj.CRN = tds[0].textContent;
-	classObj.course = tds[1].textContent;
-	classObj.title = tds[2].textContent;
-	classObj.time = tds[3].textContent.split("\n");
-	classObj.room = tds[4].textContent;
-	classObj.instructor = tds[5].textContent;
-	classObj.seatsAvail = tds[6].textContent;
-	classObj.seatsRes = tds[7].textContent;
-	classObj.prm = tds[8].textContent;
-	classObj.CCC = tds[9].textContent;
-	console.log(classObj);
+	classObj.CRN = tds[0].textContent.trim();
+	classObj.course = tds[1].textContent.trim();
+	classObj.title = tds[2].textContent.trim();
+	classObj.time = tds[3].textContent.replace(/\n/g, "|");
+	classObj.room = tds[4].textContent.replace(/\n/g, "|");
+	classObj.instructor = tds[5].textContent.replace(/\n/g, "");
+	classObj.seatsAvail = tds[6].textContent.trim();
+	classObj.seatsRes = tds[7].textContent.trim();
+	classObj.prm = tds[8].textContent.trim();
+	classObj.CCC = tds[9].textContent.trim();
+	console.log(JSON.stringify(classObj));
 });
 
 // Minified sidebar
@@ -51,3 +51,16 @@ $(".atcbutton").click(function(){
 // Adding the sidebar div
 //$("body").wrapInner("<div class='original'></div>");
 //$(".original").after(sidebarHTML);
+
+//var exClasses = {
+//    0: {"CRN":"17153","course":"ACFM 220 01","title":"Business Law I","time":"|MWF 2:00-2:52pm|R 7:00-9:52pm|","room":"|TAYL 113||","instructor":"Brann, Paul W.","seatsAvail":"Closed","seatsRes":"","prm":"","CCC":"SLSC"},
+//    1: {"CRN":"10108","course":"BIOL 121 01","title":"Biology for Non-majors","time":"|MWF 11:00-11:52am|","room":"|VAUG 100|","instructor":"Gates, Julie A.","seatsAvail":"9","seatsRes":"","prm":"","CCC":"FRST LBSC NMLG NSMC"},
+//    2: {"CRN":"17235","course":"CHIN 101R 42","title":"Recitation-Chinese I","time":"|TR 4:00-4:52pm|","room":"|OLIN 371|","instructor":"STAFF","seatsAvail":"1","seatsRes":"","prm":"","CCC":""},
+//    3: {"CRN":"18347","course":"EDUC 305 01","title":"Advanced Educational Psych","time":"|TR 1:00-2:22pm|","room":"|OLIN 275|","instructor":"Nottis, Katharyn E.","seatsAvail":"18","seatsRes":"","prm":"","CCC":"SLSC"},
+//    4: {"CRN":"18350","course":"EDUC 339 01","title":"Inclusive Practices","time":"|TR 1:00-2:22pm|","room":"|OLIN 255|","instructor":"Hoffman, Lynn M.","seatsAvail":"20","seatsRes":"","prm":"","CCC":"SLSC"},
+//    5: {"CRN":"18641","course":"CSCI 203 04","title":"Intro to Computer Science I","time":"|MWF 3:00-3:52pm|","room":"|BRKI 065|","instructor":"Dancy, Christopher (Chris) L.","seatsAvail":"9","seatsRes":"","prm":"","CCC":"FRST NMLG NSMC"},
+//    6: {"CRN":"13055","course":"CSCI 479 01","title":"Computer ScienceDesign Project","time":"|MWF 3:00-3:52pm|","room":"|BRKI 165|","instructor":"Meng, Xiannong","seatsAvail":"6","seatsRes":"","prm":"","CCC":"W2"},
+//    7: {"CRN":"18614","course":"MATH 208 01","title":"Mathematical Explorations","time":"|R 9:30-10:52am|","room":"|OLIN 371|","instructor":"Piggott, Adam","seatsAvail":"15","seatsRes":"","prm":"","CCC":"FRST NSMC"},
+//    8: {"CRN":"10329","course":"PSYC 209 01","title":"Social Psychology","time":"|MWF 11:00-11:52am|","room":"|OLRY 201|","instructor":"Wade, T. Joel","seatsAvail":"Closed","seatsRes":"","prm":"","CCC":"EGSS SLSC SSLG"},
+//    9: {"CRN":"17306","course":"SPAN 103 02","title":"Toward Intermediate Spanish","time":"|MWF 3:00-3:52pm|","room":"|VAUG 102|","instructor":"Poust, Alice J.","seatsAvail":"1","seatsRes":"1","prm":"","CCC":"ARHC CCFL EGHU FRST GLSP"}
+//}
