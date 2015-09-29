532295a56ce0c73a14744e5b314e642073c46514
erc009 <erc009@bucknell.edu>
2015-09-28 22:09:12 -0400

    working on other tabs

--- sidebar/script.js
+++ sidebar/script.js
@@ -10,7 +10,25 @@ $(document).ready(function() {
         // Change/remove current tab to active
         //jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
+
         //e.preventDefault();
 	//});
 
+        e.preventDefault();
+    });
+    
+jQuery('.port .port-links a').on( 'click', function(e) {
+    var currentAttrValue = jQuery(this).attr('href');
+
+    // Show/Hide tabs
+    jQuery( '.port ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+    // Change/remove current tab to active
+    jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+    e.preventDefault();
 });
+    
+   
+
+
