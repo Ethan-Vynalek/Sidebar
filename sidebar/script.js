e6c552d3ed3c15b72a6ca084bf86afcee7d91472
erc009 <erc009@bucknell.edu>
2015-10-02 15:35:40 -0400

    Finished import/export buttons

--- sidebar/script.js
+++ sidebar/script.js
@@ -1,34 +1,28 @@
 $(document).ready(function() {
 
-	$("#sidebar").accordion();
-	//jQuery('.tabs .tab-links a').on( 'click', function(e) {
-        //var currentAttrValue = jQuery(this).attr('href');
-
-        // Show/Hide tabs
-        //jQuery( '.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
-
-        // Change/remove current tab to active
-        //jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-
-
-        //e.preventDefault();
-	//});
-
-        e.preventDefault();
-    });
-    
-jQuery('.port .port-links a').on( 'click', function(e) {
-    var currentAttrValue = jQuery(this).attr('href');
+    $("#sidebar").accordion();
+    //jQuery('.tabs .tab-links a').on( 'click', function(e) {
+    //var currentAttrValue = jQuery(this).attr('href');
 
     // Show/Hide tabs
-    jQuery( '.port ' + currentAttrValue).fadeIn(400).siblings().hide();
+    //jQuery( '.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
 
     // Change/remove current tab to active
-    jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+    //jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
 
-    e.preventDefault();
-});
-    
+
+    //e.preventDefault();
+    //});
+
+    //e.preventDefault();
    
+    console.log("hello");
+    document.getElementById('export-button').onclick = function() {
+        window.confirm("You are exporting");
+    }
 
-
+      document.getElementById('import-button').onclick = function() {
+        window.confirm("You are importing");
+    }
+    
+ });
