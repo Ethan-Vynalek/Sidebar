49f1cad3b02f026779f4b56010986d24562408cb
pobb001 <pobb001@bucknell.edu>
2015-10-26 16:18:47 -0400

    hosting at Myjson

--- search-logic/logic.js
+++ search-logic/logic.js
@@ -2,9 +2,9 @@
 // {0:{CRN:"17153",course:"ACFM 220 01",title:"Business Law I",time:"|MWF 2:00-2:52pm|R 7:00-9:52pm|",room:"|TAYL 113||",instructor:"Brann, Paul W.",seatsAvail:"Closed",seatsRes:"",prm:"",CCC:"SLSC"},1:{CRN:"10108",course:"BIOL 121 01",title:"Biology for Non-majors",time:"|MWF 11:00-11:52am|",room:"|VAUG 100|",instructor:"Gates, Julie A.",seatsAvail:"9",seatsRes:"",prm:"",CCC:"FRST LBSC NMLG NSMC"},2:{CRN:"17235",course:"CHIN 101R 42",title:"Recitation-Chinese I",time:"|TR 4:00-4:52pm|",room:"|OLIN 371|",instructor:"STAFF",seatsAvail:"1",seatsRes:"",prm:"",CCC:""},3:{CRN:"18347",course:"EDUC 305 01",title:"Advanced Educational Psych",time:"|TR 1:00-2:22pm|",room:"|OLIN 275|",instructor:"Nottis, Katharyn E.",seatsAvail:"18",seatsRes:"",prm:"",CCC:"SLSC"},4:{CRN:"18350",course:"EDUC 339 01",title:"Inclusive Practices",time:"|TR 1:00-2:22pm|",room:"|OLIN 255|",instructor:"Hoffman, Lynn M.",seatsAvail:"20",seatsRes:"",prm:"",CCC:"SLSC"},5:{CRN:"18641",course:"CSCI 203 04",title:"Intro to Computer Science I",time:"|MWF 3:00-3:52pm|",room:"|BRKI 065|",instructor:"Dancy, Christopher (Chris) L.",seatsAvail:"9",seatsRes:"",prm:"",CCC:"FRST NMLG NSMC"},6:{CRN:"13055",course:"CSCI 479 01",title:"Computer ScienceDesign Project",time:"|MWF 3:00-3:52pm|",room:"|BRKI 165|",instructor:"Meng, Xiannong",seatsAvail:"6",seatsRes:"",prm:"",CCC:"W2"},7:{CRN:"18614",course:"MATH 208 01",title:"Mathematical Explorations",time:"|R 9:30-10:52am|",room:"|OLIN 371|",instructor:"Piggott, Adam",seatsAvail:"15",seatsRes:"",prm:"",CCC:"FRST NSMC"},8:{CRN:"10329",course:"PSYC 209 01",title:"Social Psychology",time:"|MWF 11:00-11:52am|",room:"|OLRY 201|",instructor:"Wade, T. Joel",seatsAvail:"Closed",seatsRes:"",prm:"",CCC:"EGSS SLSC SSLG"},9:{CRN:"17306",course:"SPAN 103 02",title:"Toward Intermediate Spanish",time:"|MWF 3:00-3:52pm|",room:"|VAUG 102|",instructor:"Poust, Alice J.",seatsAvail:"1",seatsRes:"1",prm:"",CCC:"ARHC CCFL EGHU FRST GLSP"}};
 var exClasses;
 // set to true to get logic debugging information
-var debug = false;
+var debug = true;
 
-$.getJSON(chrome.extension.getURL('search-logic/courseList.JSON'), function(response) {
+$.getJSON("https://api.myjson.com/bins/2l7mc", function(response) {
     exClasses = response;
     if(debug) {
         console.log("CRNSearch");
